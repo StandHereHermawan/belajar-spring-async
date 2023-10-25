5e3485d87d3bdde4331e8bb4bdf60d133ed0fd19
StandHereHermawan <nolifegansaya@gmail.com>
2023-10-25 12:25:49 +0700

    Enable Async, Initial commit

---
+++ src/main/java/ariefbelajarteknologi/springasync/BelajarSpringAsyncApplication.java
@@ -0,0 +1,15 @@
+package ariefbelajarteknologi.springasync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@EnableAsync
+@SpringBootApplication
+public class BelajarSpringAsyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BelajarSpringAsyncApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ariefbelajarteknologi/springasync/BelajarSpringAsyncApplicationTests.java
@@ -0,0 +1,13 @@
+package ariefbelajarteknologi.springasync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelajarSpringAsyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
