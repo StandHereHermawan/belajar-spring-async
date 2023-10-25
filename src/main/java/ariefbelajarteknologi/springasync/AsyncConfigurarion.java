808bdfeed5ffbf24db99d4239f743b21dbe3e8ae
StandHereHermawan <nolifegansaya@gmail.com>
2023-10-25 15:55:10 +0700

    Default Thread Pool, add configuration class

---
+++ src/main/java/ariefbelajarteknologi/springasync/AsyncConfigurarion.java
@@ -0,0 +1,16 @@
+package ariefbelajarteknologi.springasync;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+@Configuration
+public class AsyncConfigurarion {
+
+    @Bean
+    public Executor taskExecutors(){
+        return Executors.newVirtualThreadPerTaskExecutor();
+    }
+}
